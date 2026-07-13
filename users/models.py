a8e32db9ee23a33825646c6aa8933f0d817dd50d
Bárbara de Figueredo Matias <barbarafigueredosilva@gmail.com>
2026-07-13 16:00:43 -0300

    feat: Add custom User model and update settings for authentication

--- users/models.py
+++ users/models.py
@@ -2,4 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    
+    cpf = models.CharField(max_length=14, unique=True)
+    email = models.EmailField(unique=True)
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+
+    def save(self, *args, **kwargs):
+        self.cpf = self.cpf.replace('.', '').replace('-', '')
+        super(User, self).save(*args, **kwargs)
