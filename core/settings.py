d0d334f510ecc7b043589fc0ef794b1279104980
Bárbara de Figueredo Matias <barbarafigueredosilva@gmail.com>
2026-07-13 17:24:00 -0300

    refactor: Remove unused User model and related settings

--- core/settings.py
+++ core/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     "users",
 ]
 
-AUTH_USER_MODEL = "users.User"
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
--- users/models.py
+++ users/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    cpf = models.CharField(max_length=14, unique=True)
-    email = models.EmailField(unique=True)
-    amount = models.DecimalField(max_digits=15, decimal_places=2)
-
-    def save(self, *args, **kwargs):
-        self.cpf = self.cpf.replace('.', '').replace('-', '')
-        super(User, self).save(*args, **kwargs)
+# Create your models here.
