10d5f5e94de031d301304ab3cb0d0a0be272630a
Bárbara de Figueredo Matias <barbarafigueredosilva@gmail.com>
2026-07-18 15:14:50 -0300

    feat: implementando model User customizado com configurações de autenticação e criação de API user

--- users/api.py
+++ users/api.py
@@ -1,8 +1,12 @@
 from ninja import Router
+from .schemas import UserSchema
+from .models import User
+users_router = Router()
 
 users_router = Router()
 
 @users_router.post("/", response={200: dict})
-def create_user(request,):
-    # logica para criar um usuário
+def create_user(request, user: UserSchema):
+    user = User(**user.dict())
+
     return {'ok': 'ok'}
