cac40f3770bf02bf65ed50cf86de073ec4d5993c
Bárbara de Figueredo Matias <barbarafigueredosilva@gmail.com>
2026-07-22 11:17:05 -0300

    feat: add user password hashing and create migration for user amount field

--- users/api.py
+++ users/api.py
@@ -1,6 +1,7 @@
 from ninja import Router
 from .schemas import UserSchema
 from .models import User
+from django.contrib.auth.hashers import make_password
 users_router = Router()
 
 users_router = Router()
@@ -8,5 +9,6 @@ users_router = Router()
 @users_router.post("/", response={200: dict})
 def create_user(request, user: UserSchema):
     user = User(**user.dict())
-
+    user.password = make_password(user.password)
+    user.save()
     return {'ok': 'ok'}
