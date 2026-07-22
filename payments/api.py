110efc7d091a9ced552f929baab079fda784a2cf
Bárbara de Figueredo Matias <barbarafigueredosilva@gmail.com>
2026-07-22 16:44:06 -0300

    feat: add TransactionSchema for transaction validation and update transaction processing endpoint

--- payments/api.py
+++ payments/api.py
@@ -1,8 +1,10 @@
 from ninja import Router
+from .schemas import TransactionSchema
 
 payments_router = Router()
 
 @payments_router.post("/")
-def transaction(request):
+def transaction(request, transaction: TransactionSchema):
+    print(transaction.dict())
     # Lógica para processar a transação
     return {"message": "Transação processada com sucesso."}
